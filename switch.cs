53566024ca64d928bf45a4254c092b82bc5f2816
Geovanna <66391080+geovannabrgs@users.noreply.github.com>
2022-05-13 10:37:11 -0300

    Update switch.cs

--- switch.cs
+++ switch.cs
@@ -1,17 +1,23 @@
 using System;
 
-int numbers = 2;
+public class Program
+{
+	public static void Main()
+	{
+		int numbers = 2;
 
-switch(numbers){
-    case 0: 
-        Console.WriteLine("zero");
-        break;
+        switch(numbers){
+            case 0: 
+                Console.WriteLine("zero");
+                break;
     
-    case 1:
-        Console.WriteLine("one");
-        break;
+             case 1:
+                Console.WriteLine("one");
+                break;
     
-    case 2:
-        Console.WriteLine("two");
-        break;
+              case 2:
+                Console.WriteLine("two");
+                break;
+              }
+	}
 }
