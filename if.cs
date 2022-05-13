4a336ef95af8cf0bc838622b5ca406147bf7e871
Geovanna <66391080+geovannabrgs@users.noreply.github.com>
2022-05-13 10:44:51 -0300

    Update if.cs

--- if.cs
+++ if.cs
@@ -4,10 +4,10 @@ public class Program
 {
 	public static void Main()
 	{
-    	if(args.Length == 0){
-        	Console.WriteLine("nenhum argumento");
-    	}else if(args.Length == 1){
-        	Console.WriteLine("um argumento");
-    	}
+    		if(args.Length == 0){
+        		Console.WriteLine("nenhum argumento");
+		}else if(args.Length == 1){
+        		Console.WriteLine("um argumento");
+    		}
 	}
 }
