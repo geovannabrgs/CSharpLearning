568ac6fd08525f053f1c0e35e503d85b5c61dfa6
Geovanna <66391080+geovannabrgs@users.noreply.github.com>
2022-05-13 10:43:29 -0300

    Update foreach.cs

--- foreach.cs
+++ foreach.cs
@@ -5,7 +5,6 @@ public class Program
 	public static void Main()
 	{
 		string[] names = {"Mary", "Alisson", "Ale", "Maya"};
-		
 		foreach (string name in names)
 		{
 			Console.WriteLine("{0}", name);
