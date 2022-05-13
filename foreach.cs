27476a54247948e2fac77a373c3074ad879da55a
Geovanna <66391080+geovannabrgs@users.noreply.github.com>
2022-05-13 10:32:25 -0300

    Create foreach.cs

---
+++ foreach.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		string[] names = {"Mary", "Alisson", "Ale", "Maya"};
+		
+		foreach (string name in names)
+		{
+			Console.WriteLine("{0}", name);
+		}
+	}
+}
