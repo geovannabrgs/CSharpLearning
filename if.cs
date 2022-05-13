721b4dd544779fd37216d7fa0299d22b0d2267fe
Geovanna <66391080+geovannabrgs@users.noreply.github.com>
2022-05-13 10:21:17 -0300

    Create if.cs

---
+++ if.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+    	if(args.Length == 0){
+        	Console.WriteLine("nenhum argumento");
+    	}else if(args.Length == 1){
+        	Console.WriteLine("um argumento");
+    	}
+	}
+}
