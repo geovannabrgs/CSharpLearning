296d8d27520637cc82e33667b7dff531c3f6addd
Geovanna <66391080+geovannabrgs@users.noreply.github.com>
2022-05-13 10:42:11 -0300

    Update while.cs

--- while.cs
+++ while.cs
@@ -4,11 +4,11 @@ public class Program
 {
 	public static void Main()
 	{
-        int i = 0;
+        	int i = 0;
 
-        while(i < 10){
-            Console.WriteLine(i);
-            i++;
-        }
+        	while(i < 10){
+            		Console.WriteLine(i);
+            		i++;
+        	}
 	}
 }
