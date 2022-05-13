02698f834cba506a2c7b57f8db31770924c34a9d
Geovanna <66391080+geovannabrgs@users.noreply.github.com>
2022-05-13 10:33:20 -0300

    Update for.cs

--- for.cs
+++ for.cs
@@ -1,5 +1,11 @@
 using System;
 
-for (int i = 0; i < 10; i++){
-    Console.WriteLine(i);
+public class Program
+{
+	public static void Main()
+	{
+        for (int i = 0; i < 10; i++){
+            Console.WriteLine(i);
+        }     
+	}
 }
