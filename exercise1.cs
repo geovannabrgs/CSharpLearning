a19a829bce9de600094878b1b6fc0d0dd0893bfa
Geovanna <66391080+geovannabrgs@users.noreply.github.com>
2022-05-16 12:11:57 -0300

    Update exercise1.cs

--- exercise1.cs
+++ exercise1.cs
@@ -13,6 +13,10 @@ namespace curso{
       perimetro = 2 * (b + a);
       diagonal = Math.Sqrt(Math.Pow(b, 2.0) + Math.Pow(a, 2.0));
       
+      Console.WriteLine("Area = " area.ToString("F4", CultureInfo.InvariantCulture));
+      Console.WriteLine("Perimetro = " perimetro.ToString("F4", CultureInfo.InvariantCulture));
+      Console.WriteLine("Diagonal = " diagonal.ToString("F4", CultureInfo.InvariantCulture));
+      
       Console.ReadLine();
     }
   }
