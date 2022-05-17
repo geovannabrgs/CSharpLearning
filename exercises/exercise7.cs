30e0ac02ec037a67729ebb7ef30a5e96d7c80c4d
Geovanna Borges <66391080+geovannabrgs@users.noreply.github.com>
2022-05-17 12:14:44 -0300

    Create exercise7.cs

---
+++ exercises/exercise7.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso
+{
+    public class Aluno
+    {
+        public string Nome {get;set;}
+        public decimal Nota {get; set;}
+    }
+}
