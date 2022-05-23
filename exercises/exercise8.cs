cd76fe4fe5c7263f6d5c17bffd0be553c4593700
Geovanna Borges <66391080+geovannabrgs@users.noreply.github.com>
2022-05-23 10:27:34 -0300

    Create exercise8.cs

---
+++ exercises/exercise8.cs
@@ -0,0 +1,14 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+	  int x;
+
+      x = int.Parse(Console.ReadLine());
+      while(x != 0){
+        x = int.Parse(Console.ReadLine());
+		}
+ 	}
+}
